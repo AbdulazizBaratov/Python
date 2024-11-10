fb0ed1563b473419b7bd9ca0522cd1e0ed99a813
AbdulazizBaratov <Foxone064@gmail.com>
2024-11-10 17:58:42 +0500

    hi

---
+++ Day 16/main.py
@@ -0,0 +1,29 @@
+import turtle
+
+abdulaziz = turtle.Turtle()
+abdulaziz.color("blue")
+abdulaziz.shape("square")
+display = turtle.Screen()
+display.bgcolor("yellow")
+abdulaziz.turtlesize(1)
+abdulaziz.fd(120)
+abdulaziz.rt(150)
+abdulaziz.fd(150)
+abdulaziz.lt(150)
+turtle.begin_fill()
+abdulaziz.bk(120)
+
+abdulaziz.lt(150)
+
+abdulaziz.fd(150)
+
+abdulaziz.rt(150)
+
+abdulaziz.fd(250)
+turtle.end_fill()
+
+
+
+
+
+turtle.done()
