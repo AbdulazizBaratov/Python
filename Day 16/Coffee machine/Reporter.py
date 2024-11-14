c957078625a48e28626117beae58902013cfe2d0
AbdulazizBaratov <Foxone064@gmail.com>
2024-11-14 18:36:08 +0500

    Day 16

--- Day 16/Coffee machine/Reporter.py
+++ Day 16/Coffee machine/Reporter.py
@@ -1,7 +1,13 @@
-import money_collector
-
-
-
 money_collector.Money_coins()
 def report():
-    Coffee_Beans = 227 
+    Coffee_Beans = 227
+    Milk = 1000
+    sugar = 1000
+    water = 10000
+    Ice = 120
+    print(f'''
+    Milk ---> {Milk}
+    sugar ---> {Coffee_Beans}
+    water ---> {water}
+    Ice ---> {Ice}
+''')
